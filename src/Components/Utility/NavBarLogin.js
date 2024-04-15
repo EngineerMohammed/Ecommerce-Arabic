1c1ab34a8d43d07e2222229919d9ec45c1ae7ae5
Engineer Mohammed <eng_abo_hussien@yahoo.com>
2024-04-15 19:05:49 +0200

    last navupdate

--- src/Components/Utility/NavBarLogin.js
+++ src/Components/Utility/NavBarLogin.js
@@ -8,24 +8,23 @@ const NavBarLogin = () => {
   return (
     <nav className=" bg-zinc-800">
       <div className="container max-w-7xl m-auto px-8 py-4">
-        <div className="lg:flex lg:flex-row flex-col justify-between items-center gap-4">
-          <div className="flex justify-between items-center w-full">
+        <div className="md:flex  md:flex-row flex-col justify-between items-center gap-4">
+          <div className="flex justify-between md:w-fit md:m-auto m-4 items-center w-full">
             <div>
               <img src={logo} alt="" width={60} height={60} />
             </div>
             <div>
-              <FaBars className="lg:hidden text-white text-3xl" />
+              <FaBars className="md:hidden text-white text-3xl" />
             </div>
           </div>
-
-          <div className="flex lg:w-[80%]  justify-center items-center">
+          <div className="flex md:w-[80%]  justify-center items-center">
             <input
               type="text"
               placeholder="ابحث...."
               className="text-center flex-1 "
             />
           </div>
-          <div className="flex md:flex-row flex-col lg:m-auto m-3 gap-4 justify-center items-center">
+          <div className="flex md:flex-row flex-col md:m-auto m-3 gap-4 justify-center items-center">
             <div className="flex gap-1 cursor-pointer justify-center items-center">
               <img src={login} width={25} height={25} alt="" />
               <p className=" text-white">دخول</p>
